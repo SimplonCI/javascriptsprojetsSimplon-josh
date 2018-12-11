a83f7b06f65e96846f79ad824419d5dfbc7642c2
Josue Tchirktema <tchirktemajosue@gmail.com>
2018-12-11 09:50:24 +0000

    debug form validation

--- javascript/main.js
+++ javascript/main.js
@@ -20,22 +20,42 @@ function validationFormulaire() {
     var telephone = document.getElementById("telephone").value;
     var ville = document.getElementById("ville").value;
     var sexe = document.querySelector('input[name="genre"]:checked').value;
+    
 
+    
     if(nom == null || nom == '' ) {
-        alert("Veuillez remplir le champs nom")
+        alert("Veuillez remplir le champs nom");
+        return false;
+       
     } else if(prenom == null || prenom == '' ) {
-        alert("Veuillez remplir le champs prenom")
+        alert("Veuillez remplir le champs prenom");
+        return false;
+        
     } else if(telephone == null || telephone == '' ) {
-        alert("Veuillez remplir le champs telephone")
+        alert("Veuillez remplir le champs telephone");
+        return false;
     } else if(ville == null || ville == '' ) {
-        alert("Veuillez remplir le champs ville")
+        alert("Veuillez remplir le champs ville");
+        return false;
     } else if(sexe == null || sexe == '' ) {
-        alert("Veuillez remplir le champs sexe")
+        alert("Veuillez remplir le champs sexe");
+        return false;
     }
-    else{
-        window.location.href = "welcome.html";
-        // alert("Nom : "+ nom+" ; Prenom : "+prenom+" ; Telephone :"+telephone+" Ville: "+ville+"; Sexe :"+sexe);
+    else if(sexe == 'masculin'){
+        
+        document.location.href = "monsieur.html";
+        // alert('dwed')
+        return false;
+        
+    } else if(sexe == 'feminin'){
+        document.location.href = "madame.html";
+        return false;
+        
+    } else{
+        alert("Oups pas de sexe!!!!");
+        return false;
     }
 
+   
     
 }
